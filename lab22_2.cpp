3647f36dd2bb39924b445119f6add17ef321aaee
thanakrit555 <59599161+thanakrit555@users.noreply.github.com>
2021-03-12 17:36:26 +0700

    done

--- lab22_2.cpp
+++ lab22_2.cpp
@@ -23,7 +23,27 @@ int main(){
 	loc = find(line_up.begin(),line_up.end(),"Bob");
 	line_up.insert(loc,"Oscar");
 	
-	//Write your code here
+line_up.push_back("Luffy");
+	line_up.push_back("Sanji");
+	line_up.push_back("Nami");
+	line_up.pop_front();
+	line_up.pop_front();
+	
+	loc = find(line_up.begin(),line_up.end(),"Luffy");
+	loc++;
+	line_up.insert(loc,"Narutu");
+	
+	line_up.push_front("Prayuth");
+	
+	loc = find(line_up.begin(),line_up.end(),"Prayuth");
+	loc++;
+	line_up.insert(loc,"Tukkie");
+	
+	loc = find(line_up.begin(),line_up.end(),"Bob");
+	line_up.erase(loc);
+	for(int i=0; i<3;i++){
+		line_up.pop_front();
+	}
 	
 	printList(line_up);
 		
